c8488fd7fb6fdd7213c009b1164320f2d7ad0939
Amanda Maharjan <maharjanamanda@gmail.com>
2019-05-08 10:01:24 +0545

    models created

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -2,16 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+GAME_STATUS_CHOICES = (
+	('F', 'First Player To Move'),
+	('S', 'Second Player To Move'),
+	('W', 'First Player Wins'),
+	('L', 'Second Player Wins'),
+	('D', 'Draw')
+)
+
+
 class Game(models.Model):
 	frist_player = models.ForeignKey(User, on_delete=models.CASCADE,
 			related_name="games_first_player"
-			)
+					)
 	second_player = models.ForeignKey(User, on_delete=models.CASCADE,
 			related_name="games_second_player"
 			)
 	start_time = models.DateTimeField(auto_now_add=True)
 	last_active = models.DateTimeField(auto_now=True)
-	status = models.CharField(max_length=1, default='F')
+	status = models.CharField(max_length=1, default='F', choices=GAME_STATUS_CHOICES)
+
+	def __str__(self):
+		return "{0} vs {1}".format(self.frist_player, self.second_player)
 
 
 class Move(models.Model):
