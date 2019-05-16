34fd2f326cad5db34fb1d7ce3363659adaff0d99
Amanda Maharjan <maharjanamanda@gmail.com>
2019-05-16 10:17:24 +0545

    active and remaining games list separated

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -11,13 +11,15 @@ from .models import Invitation
 def home(request):
 	my_games = Game.objects.games_for_user(request.user)
 	active_games = my_games.active()
+	finished_games = my_games.difference(active_games)
 	invitations = request.user.invitations_received.all()
 
 	return render(
 		request,
 		"player/home.html",
-		{'game': active_games,
-			'invitations': invitations}
+		{'active_games': active_games,
+			'invitations': invitations,
+			'finished_games': finished_games}
 	)
 
 
